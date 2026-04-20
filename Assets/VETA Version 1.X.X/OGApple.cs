daf628379d4b5a53b1b38e2c3f8f488b88038657
Airless-KV <sebastiankahrdvirina29@gmail.com>
2026-04-20 22:31:20 +0800

    Kahrd's Prog 4/20/2026
    - removed adaptive performace folder
    - re-named "Sample Scene" to TM1_MainTestScene
        - re-worked the scene adding multiple elements
    - added two new test maps TM2_FlatTestScene and TM3_SphereTestScene
        - specific scenes for OGApple and _AppleHandler
    - changed comments
    - added two new materrials
        - purple used for Assets_Plate and white for Sphere_Plane/Sphere's

--- Assets/VETA Version 1.X.X/OGApple.cs
+++ Assets/VETA Version 1.X.X/OGApple.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
-// Version 1.?: Initial implementation of the apple behavior. 4/18/2026
+// Version 1.2: Initial implementation of the apple behavior. 4/18/2026
 // Abbandoned for the 3D sphere/ramp math, but left in the project for reference. 4/18/2026
-// Version 1.?.1: Cleaned up, commented, renamed, and refactored version of the original Apple script. 4/19/2026
+// Version 1.2.1: Cleaned up, commented, renamed, and refactored version of the original Apple script. 4/20/2026
 
 // =========================================================================
 // COMPATIBILITY: OGApple is mainly used in VETA Version 1.X.X scripts.
@@ -9,9 +9,6 @@ using UnityEngine;
 // =========================================================================
 
 // =========================================================================
-// NOTE: Other apple scripts SHOULD BE TURNED OFF multiple scrips will cause conflicts. To turn the 2D flat level back on w/ unityGravity remove the comment below.
-// DO NOT REMOVE the other apple scripts. Just comment them out!
-// See _AppleHandler.cs for clarification on why this is necessary.
 // NOTE: Change the component name to match the snake tail handler you are using, if you switch to a different one.
 // =========================================================================
 
@@ -19,26 +16,28 @@ using UnityEngine;
 // This script handles the behavior of the apple, including detecting when the snake eats it and randomizing its position.
 public class OGApple : MonoBehaviour
 {
-    //// This method is called when another collider enters the trigger collider attached to the apple.
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    Debug.Log("The apple was touched by: " + other.gameObject.name);
+    public float spawnRange = 23f; 
 
-    //    if (other.CompareTag("snakeHead_Player"))
-    //    {
-    //        other.GetComponent<OGSnake>().Grow();
-    //        RandomizePosition();
-    //    }
-    //}
+    // This method is called when another collider enters the trigger collider attached to the apple.
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("The apple was touched by: " + other.gameObject.name);
 
-    //// This method will move the apple to a random position within a 20x20 area centered around the origin (0,0,0).
-    //private void RandomizePosition()
-    //{
+        if (other.CompareTag("snakeHead_Player"))
+        {
+            other.GetComponent<OGSnake>().Grow();
+            RandomizePosition();
+        }
+    }
 
-    //    float randomX = Random.Range(-10f, 10f);
-    //    float randomZ = Random.Range(-10f, 10f);
+    // This method will move the apple to a random position within a 20x20 area centered around the origin (0,0,0).
+    private void RandomizePosition()
+    {
 
-    //    // Move the apple. but keep the Y coordinate at 0 to stay on the ground
-    //    transform.position = new Vector3(randomX, 0f, randomZ);
-    //}
+        float randomX = Random.Range(spawnRange - spawnRange * 2, spawnRange);
+        float randomZ = Random.Range(spawnRange - spawnRange * 2, spawnRange);
+
+        // Move the apple. but keep the Y coordinate at 0 to stay on the ground
+        transform.position = new Vector3(randomX, 0f, randomZ);
+    }
 }  
--- Assets/VETA Version 2.X.X/_AppleHandler.cs
+++ Assets/VETA Version 2.X.X/_AppleHandler.cs
@@ -8,17 +8,7 @@ using UnityEngine;
 // =========================================================================
 
 // =========================================================================
-// NOTE: Other apple scripts SHOULD BE TURNED OFF! multiple scrips will cause conflicts. To turn the 3D Sphere/Ramp math back on remove the comment below.
-// DO NOT REMOVE the other apple scripts. Just comment them out!
 // NOTE: Change the component name to match the snake tail handler you are using, if you switch to a different one.
-// =========================================================================
-
-// =========================================================================
-// CLARIFICATION: ON NEEDING TO COMMENT OUT OTHER APPLE SCRIPTS
-// If you have multiple Apple scripts in your project, they will all try to run at the same time and cause conflicts.
-// Unchecking a script in Unity only stops its frame-by-frame updates, but it does not disable its physics triggers.
-// Meaning the code still fires the exact millisecond something collides with it.
-// it is the developer decision to leave the other Apple scripts in the component list for reference, but they must be commented out to prevent conflicts.
 // =========================================================================
 
 
@@ -35,7 +25,7 @@ public class AppleHandler : MonoBehaviour
 
         if (other.CompareTag("snakeHead_Player"))
         {
-            other.GetComponent<OGSnake>().Grow();
+            other.GetComponent<OGSnakeTailHandler>().Grow();
             RandomizePosition();
         }
     }
